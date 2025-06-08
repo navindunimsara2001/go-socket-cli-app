b226e23c52c21869538944e965eb41fc2fc894c3
navindunimsara2001 <navidunimsara@gmail.com>
2025-06-08 10:07:56 +0530

    updated server for username requirement

--- server/main.go
+++ server/main.go
@@ -4,12 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"strings"
 )
 
-// map to store connected clients
 var clients = make(map[net.Conn]string)
-
-// channel to handle incoming messages form clients
 var messages = make(chan string)
 
 func main() {
@@ -19,12 +17,10 @@ func main() {
 		fmt.Println("Error starting server: ", err)
 		return
 	}
-
 	defer listener.Close()
 
-	fmt.Println("Server started successfully. Listening on port" + listener.Addr().String() + " " + listener.Addr().Network())
+	fmt.Println("Server started successfully. Listening on", listener.Addr().String())
 
-	// A goroutine is started to handle broadcasting messages to all clients.
 	go broadcastMessages()
 
 	for {
@@ -35,49 +31,43 @@ func main() {
 		}
 		go handleClient(conn)
 	}
-
 }
 
 func broadcastMessages() {
 	for {
-		msg := <-messages //receive a message from the channel
+		msg := <-messages
 		for conn := range clients {
-			//
-			_, err := conn.Write([]byte(msg + "\n"))
+			_, err := fmt.Fprintln(conn, msg)
 			if err != nil {
-				fmt.Println("Error broadcasting message: ", err)
+				fmt.Println("Error broadcasting to", clients[conn], ":", err)
 			}
 		}
 	}
 }
 
 func handleClient(conn net.Conn) {
-	conn.Write([]byte("Please enter your username: "))
-
+	fmt.Fprint(conn, "Please enter your username: ")
 	username, err := bufio.NewReader(conn).ReadString('\n')
-
 	if err != nil {
-		fmt.Println("Error reading username: ", err)
+		fmt.Println("Error reading username:", err)
 		conn.Close()
 		return
 	}
-
-	username = username[:len(username)-1]
-	// The new client and their username are added to the clients map.
+	username = strings.TrimSpace(username)
 	clients[conn] = username
-	messages <- fmt.Sprint("%s has joined the chat", username)
 
-	// read message from client
+	messages <- fmt.Sprintf("%s has joined the chat", username)
+
 	for {
 		message, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
-			// if there is an error
+			fmt.Println(username, "disconnected.")
 			delete(clients, conn)
-			// A message is broadcasted to inform others that the user has left.
 			messages <- fmt.Sprintf("%s has left the chat", username)
 			conn.Close()
 			return
 		}
+		message = strings.TrimSpace(message)
 		messages <- fmt.Sprintf("%s: %s", username, message)
 	}
 }
